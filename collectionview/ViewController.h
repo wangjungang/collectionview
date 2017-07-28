4ca2926747b0568b9fb61f3e8e429e5f170dd743
wangjungang1994 <1499370499@qq.com>
2017-07-28 18:53:55 +0800

    add

---
+++ collectionview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionview
+//
+//  Created by 王俊钢 on 2017/7/28.
+//  Copyright © 2017年 wangjungang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collectionview/ViewController.m
@@ -0,0 +1,105 @@
+//
+//  ViewController.m
+//  collectionview
+//
+//  Created by 王俊钢 on 2017/7/28.
+//  Copyright © 2017年 wangjungang. All rights reserved.
+//
+
+#define fDeviceWidth ([UIScreen mainScreen].bounds.size.width)
+#define fDeviceHeight ([UIScreen mainScreen].bounds.size.height)
+
+#import "ViewController.h"
+
+@interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@property (nonatomic,strong) UICollectionView *collectionView;
+
+@end
+
+static float AD_height = 150;//头部高度
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.collectionView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 创建collectionView并设置代理
+- (UICollectionView *)collectionView
+{
+    if (_collectionView == nil) {
+        
+        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+        
+        flowLayout.headerReferenceSize = CGSizeMake(fDeviceWidth, AD_height+10);//头部大小
+        
+        _collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, fDeviceWidth, fDeviceHeight) collectionViewLayout:flowLayout];
+        
+        //定义每个UICollectionView 的大小
+        flowLayout.itemSize = CGSizeMake((fDeviceWidth-20)/2, (fDeviceWidth-20)/2+50);
+        //定义每个UICollectionView 横向的间距
+        flowLayout.minimumLineSpacing = 5;
+        //定义每个UICollectionView 纵向的间距
+        flowLayout.minimumInteritemSpacing = 0;
+        //定义每个UICollectionView 的边距距
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 5, 5, 5);//上左下右
+        
+        //注册cell和ReusableView（相当于头部）
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
+        [_collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"ReusableView"];
+        
+        //设置代理
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        
+        //背景颜色
+        _collectionView.backgroundColor = [UIColor whiteColor];
+        //自适应大小
+        _collectionView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        
+    }
+    return _collectionView;
+}
+
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+
+#pragma mark 每个UICollectionView展示的内容
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identify = @"cell";
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identify forIndexPath:indexPath];
+    [cell sizeToFit];
+    
+    cell.backgroundColor = [UIColor greenColor];
+    return cell;
+}
+
+#pragma mark 头部显示的内容
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:
+                                            UICollectionElementKindSectionHeader withReuseIdentifier:@"ReusableView" forIndexPath:indexPath];
+    
+    headerView.backgroundColor = [UIColor redColor];
+    return headerView;
+}
+
+
+
+
+@end
